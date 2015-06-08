83c32c00f4f13bffb34fdc744514548c77c9cf5d
Michael Ficarra <git@michael.ficarra.me>
2015-06-08 04:37:20 -0700

    WIP: add enemy generation

--- src/Main.purs
+++ src/Main.purs
@@ -6,19 +6,23 @@ import DOM (DOM(), Node())
 import Signal (foldp, runSignal, sampleOn, Signal())
 import Signal.DOM (animationFrame, keyPressed)
 
-import Mario (marioLogic, charSpriteDescriptor, Character(), SpriteDescriptor(), Direction(Left, Right))
+import Mario (marioLogic, charSpriteDescriptor, enemySpriteDescriptor, enemyCharacter, makeRandomEnemy, enemyLogic, Character(), SpriteDescriptor(), Direction(Left, Right), Enemy())
 
-type GameState = { mario :: Character }
+type GameState = { mario :: Character, enemies :: [Enemy] }
 
 groundHeight = 40 -- px
 leftKeyCode = 37
 rightKeyCode = 39
 jumpKeyCode = 38
+maxEnemies = 3
 
 
 foreign import updatePositionP """
   function updatePositionP(c) {
     return function() {
+      if (c.node.parentNode == null) {
+        document.body.insertBefore(c.node, document.body.firstChild);
+      }
       c.node.style.left = c.x + "px";
       c.node.style.bottom = c.y + "px";
     };
@@ -43,6 +47,34 @@ foreign import updateSpriteP """
 updateSprite :: forall eff. Character -> Eff _ Unit
 updateSprite c = updateSpriteP c.node (charSpriteDescriptor c)
 
+foreign import data Trace :: !
+foreign import trace """
+  function trace(x) {
+    return function() {
+      window.state = x;
+      //console.dir(x);
+    };
+  }
+  """ :: forall a eff. a -> forall r. Eff r Unit
+
+foreign import atRandomInterval """
+  function atRandomInterval(next) {
+  return function(min) {
+  return function(max) {
+    function t(){ return min + (Math.random() * (max - min)); }
+    var a = [];
+    setTimeout(function iterate() {
+      a = [next()];
+      setTimeout(iterate, t());
+    }, t());
+    return function() {
+      var b = a;
+      a = [];
+      return b;
+    };
+  };};}
+  """ :: forall a eff. Eff (|eff) a -> Number -> Number -> Eff (|eff) [a]
+
 foreign import onDOMContentLoaded """
   function onDOMContentLoaded(action) {
     if (document.readyState === "interactive") {
@@ -58,6 +90,32 @@ foreign import getMarioNode """
   function getMarioNode() { return document.getElementById("mario"); }
   """ :: forall eff. Eff (dom :: DOM | eff) Node
 
+foreign import removeNode """
+  function removeNode(node) {
+    return function() {
+      node.parentNode.removeChild(node);
+    };
+  }
+  """ :: forall eff. Node -> Eff (dom :: DOM | eff) Unit
+
+cleanupOffscreenEnemies :: GameState -> Eff _ GameState
+cleanupOffscreenEnemies s = do
+  let oobEnemies = filter cond s.enemies
+  for oobEnemies \enemy -> removeNode (enemyCharacter enemy).node
+  return $ if length oobEnemies == 0 then s else s { enemies = filter (not <<< cond) s.enemies }
+    where
+      cond enemy =
+        let c = enemyCharacter enemy in
+        case c.dir of
+          Left -> c.x < 50
+          Right -> c.x > 500
+
+addEnemy :: Enemy -> GameState -> GameState
+addEnemy x s = s { enemies = x : s.enemies }
+
+enemyGenerator :: Eff _ [Enemy]
+enemyGenerator = atRandomInterval makeRandomEnemy 10000 10000
+
 
 initialState :: Node -> GameState
 initialState marioNode = {
@@ -68,17 +126,26 @@ initialState marioNode = {
     dx: 3,
     dy: 6,
     dir: Right
-  }
+  },
+  enemies: []
 }
 
 gameLogic :: { left :: Boolean, right :: Boolean, jump :: Boolean } -> Eff _ GameState -> Eff _ GameState
 gameLogic inputs gameState = do
   gs <- gameState
-  return (gs { mario = marioLogic inputs gs.mario })
+  gs' <- if length gs.enemies >= maxEnemies then return gs else do
+    newEnemies <- enemyGenerator
+    return $ foldr addEnemy gs $ take (maxEnemies - length gs.enemies) newEnemies
+  let gs'' = gs' { mario = marioLogic inputs gs'.mario, enemies = enemyLogic <$> gs'.enemies }
+  return $ cleanupOffscreenEnemies gs''
 
 render :: Eff _ GameState -> Eff _ Unit
 render gameState = do
   gs <- gameState
+  for gs.enemies \enemy -> do
+    let c = enemyCharacter enemy
+    updatePosition c
+    updateSpriteP c.node (enemySpriteDescriptor enemy)
   updatePosition gs.mario
   updateSprite gs.mario
 
--- src/Mario.purs
+++ src/Mario.purs
@@ -2,6 +2,7 @@ module Mario where
 
 import Prelude ((<<<), (*), (+), (-), (==), (<), (>), (<=), (<>), (&&), not)
 import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Random (random, randomInt, randomRange)
 import DOM (DOM(), Node())
 import Math (abs, max, min)
 
@@ -16,6 +17,7 @@ type Character = {
 data Direction = Left | Right
 type SpriteDescriptor = String
 data Activity = Walking | Standing | Jumping
+data Enemy = Goomba Character
 
 gravity = 0.15 -- px / frame^2
 maxMoveSpeed = 2.5 -- px / frame
@@ -26,6 +28,18 @@ airFriction = 0.02 -- px / frame^2
 jumpCoefficient = 0.8 -- px / frame^3
 minJumpSpeed = 4 -- px / frame
 
+
+foreign import createEnemyNode """
+  function createEnemyNode() {
+    return document.createElement("div");
+  }
+  """ :: forall eff. Eff (dom :: DOM | eff) Node
+
+makeRandomEnemy :: Eff _ Enemy
+makeRandomEnemy = do
+  node <- createEnemyNode
+  x <- randomInt 0 200
+  return $ Goomba { node: node, x: x, dx: 0, y: 500, dy: 0, dir: Right }
 
 charSpriteDescriptor :: Character -> SpriteDescriptor
 charSpriteDescriptor c = "character " <> activityDesc (currentActivity c) <> " " <> dirDesc c.dir
@@ -43,6 +57,12 @@ charSpriteDescriptor c = "character " <> activityDesc (currentActivity c) <> " "
   currentActivity c | isAirborne c = Jumping
   currentActivity c | c.dx == 0 = Standing
   currentActivity _ = Walking
+
+enemySpriteDescriptor :: Enemy -> SpriteDescriptor
+enemySpriteDescriptor e = "enemy goomba " ++ charSpriteDescriptor (enemyCharacter e)
+
+enemyCharacter :: Enemy -> Character
+enemyCharacter (Goomba s) = s
 
 isAirborne :: Character -> Boolean
 isAirborne c = c.y > 0
@@ -83,3 +103,6 @@ jump _ c = c
 
 marioLogic :: { left :: Boolean, right :: Boolean, jump :: Boolean } -> Character -> Character
 marioLogic inputs = velocity <<< applyGravity <<< walk inputs.left inputs.right <<< jump inputs.jump
+
+enemyLogic :: Enemy -> Enemy
+enemyLogic (Goomba c) = Goomba $ (velocity <<< applyGravity) c
